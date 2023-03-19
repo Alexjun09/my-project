169dafa2f3918aa10b062edaa6e52e231034ae0b
AlexJun09 <ajunyent00@gmail.com>
2023-03-20 00:18:04 +0100

    index finished + swap started

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#15803D",
+
+          secondary: "#86EFAC",
+
+          tertiary: "#CA8A04",
+
+          yellow2: "#191D24",
+
+          "base-100": "#2A303C",
+
+          info: "#3ABFF8",
+
+          success: "#36D399",
+
+          warning: "#FBBD23",
+
+          error: "#F87272",
+        },
+      },
+    ],
+  },
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
@@ -8,5 +33,5 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
-}
+  plugins: [require("daisyui")],
+};
